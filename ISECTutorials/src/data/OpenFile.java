eb07e45cebdac94bc7f44e65710c0a83d9ab640c
Francisco Dinis <mokey95@users.noreply.github.com>
2017-12-02 20:18:45 +0000

    Test

--- ISECTutorials/src/data/OpenFile.java
+++ ISECTutorials/src/data/OpenFile.java
@@ -1,9 +1,16 @@
 package data;
 
+import java.io.File;
+
 public class OpenFile {
 
     public OpenFile() {
         System.out.println("This is data starting!");
+        
+        File dir = new File("./test");
+        File[] content = dir.listFiles();
+        for(File f : content)
+            System.out.println(f.getAbsolutePath());
            
     }   
 }
