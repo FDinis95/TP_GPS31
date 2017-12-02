3d5fe2547ed29ac42051c59e6dd278387af59651
simaohsferreira <simaohsferreira@gmail.com>
2017-12-02 21:23:42 +0000

    Question update on data 2

--- ISECTutorials/src/data/Question.java
+++ ISECTutorials/src/data/Question.java
@@ -1,5 +1,34 @@
 package data;
 
 public class Question {
+    private String question;
+    private String answers[];
+    private String correct;
+    private int index;
     
+    Question(String string[]){
+        this.question = string[0];
+        this.correct = string[5];
+        for (int i=1; i<5; i++){
+            this.answers[i-1]=string[i];
+            if(string[i].equals(correct))
+                this.index=i-1;
+        }
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public String[] getAnswers() {
+        return answers;
+    }
+
+    public String getCorrect() {
+        return correct;
+    }
+
+    public int getIndex() {
+        return index;
+    }
 }
