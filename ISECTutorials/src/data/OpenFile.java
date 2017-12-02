54dc32fa8797ae9d0bd9e12921e74c737ec01291
Francisco Dinis <mokey95@users.noreply.github.com>
2017-12-02 19:46:29 +0000

    Remove unnecesarry lines

--- ISECTutorials/src/data/OpenFile.java
+++ ISECTutorials/src/data/OpenFile.java
@@ -1,21 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package data;
 
-/**
- *
- * @author simao
- */
 public class OpenFile {
 
     public OpenFile() {
         System.out.println("This is data starting!");
            
-    }
-    
-    
-    
+    }   
 }
