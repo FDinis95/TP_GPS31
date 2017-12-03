eacb412bf87215976e53282b304f7865d0e7ff66
simaohsferreira <simaohsferreira@gmail.com>
2017-12-03 19:41:04 +0000

    LoadQuestions update to individual question

--- ISECTutorials/src/data/LoadQuestions.java
+++ ISECTutorials/src/data/LoadQuestions.java
@@ -4,41 +4,54 @@ import java.io.*;
 import java.util.List;
 
 public class LoadQuestions {
-    
+
     private String diagnose;
     private List<Question> question;
-//    private String chapters;
-//    private String test;
     String line = null;
-    
+    String array[] = new String[6];
+
     public LoadQuestions(String path) {
-        
+
 //        System.out.println(path);
-        if(path == null)
+        if (path == null) {
             return;
-        
-        diagnose = path + "\\1.txt";
-        
+        }
+
+        diagnose = path + "\\diagnose.txt";
+
         System.out.println(diagnose);
-        
-        try{
-            FileReader fr = new FileReader(diagnose);
+
+        try {
+            int index = 0;
+            int questionNumber = 60;
             
-            BufferedReader br = new BufferedReader(fr);
-            
-            while((line = br.readLine()) != null){
-                System.out.println(line);
+            if(questionNumber<0 || questionNumber > 49){
+                System.err.println("Question number invalid");
+                //must validate between 0 and 12
+                //and between 0 and 49 accordind to type...
             }
             
+            FileReader fr = new FileReader(diagnose);
+            BufferedReader br = new BufferedReader(fr);
+
+            while ((line = br.readLine()) != null) {
+                if (line.contains("Q" + questionNumber + " ")) {
+                    array[index] = line;
+                    System.out.println(array[index]);
+                    index++;
+                    for (int i = 1; i < 6; i++) {
+                        array[index] = br.readLine();
+                        System.out.println(array[index]);
+                        index++;
+                    }
+                }
+            }
             br.close();
-        }
-        catch(FileNotFoundException ex){
-            System.out.println("Unable to open file");
-        }
-        
-        catch(IOException ex){
-            System.out.println("Error reading file");
+        } catch (FileNotFoundException ex) {
+            System.err.println("Unable to open file");
+        } catch (IOException ex) {
+            System.err.println("Error reading file");
         }
     }
-    
+
 }
--- ISECTutorials/src/isectutorials/ISECTutorials.java
+++ ISECTutorials/src/isectutorials/ISECTutorials.java
@@ -2,9 +2,6 @@ package isectutorials;
 
 import data.FindPath;
 import data.LoadQuestions;
-import data.OpenFile;
-import java.util.Arrays;
-import logic.UpdateProgression;
 
 public class ISECTutorials {
 
@@ -19,7 +16,6 @@ public class ISECTutorials {
         
         System.out.println("This is main!");
         
-//        new OpenFile();
 //        new UpdateProgression();
         LoadQuestions lq = new LoadQuestions(Sfp[i]);
     }
