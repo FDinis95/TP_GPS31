0816e7ae3a0714a80a7cde8edd471ed881a93516
Francisco Dinis <mokey95@users.noreply.github.com>
2017-12-03 17:37:43 +0000

    Tests

--- ISECTutorials/src/data/LoadQuestions.java
+++ ISECTutorials/src/data/LoadQuestions.java
@@ -1,30 +1,44 @@
 package data;
 
-import java.io.File;
+import java.io.*;
+import java.util.List;
 
 public class LoadQuestions {
     
     private String diagnose;
-    private String chapters;
-    private String test;
+    private List<Question> question;
+//    private String chapters;
+//    private String test;
+    String line = null;
     
     public LoadQuestions(String path) {
         
-        System.out.println(path);
+//        System.out.println(path);
         if(path == null)
             return;
         
-        diagnose = path + "\\Diagnose.txt";
+        diagnose = path + "\\1.txt";
         
         System.out.println(diagnose);
         
-        File dir = new File(path);
-        File[] content = dir.listFiles();
-        
-        for(File f : content){
-            System.out.println(f);
+        try{
+            FileReader fr = new FileReader(diagnose);
+            
+            BufferedReader br = new BufferedReader(fr);
+            
+            while((line = br.readLine()) != null){
+                System.out.println(line);
+            }
+            
+            br.close();
+        }
+        catch(FileNotFoundException ex){
+            System.out.println("Unable to open file");
         }
         
+        catch(IOException ex){
+            System.out.println("Error reading file");
+        }
     }
     
 }
