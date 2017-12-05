d4aee9e6a980cf672ffeaafec90e777410dc66c1
murilobritojr <murilinho888@gmail.com>
2017-12-05 19:29:37 +0000

    ValidateTestResult with some alteration but not done yet...

--- ISECTutorials/src/logic/ValidateTestResult.java
+++ ISECTutorials/src/logic/ValidateTestResult.java
@@ -9,18 +9,23 @@ public class ValidateTestResult {
 
     public int validateEvaluationTest(int answers[], int corrects[], int chapter[]) {
         int n = 0;
+        int chap[] = null;
         if (answers.length != 20 || corrects.length != 20 || chapter.length != 20) {
             for (int i = 0; i < answers.length; i++) {
                 if (answers[i] == corrects[i]) {
+                    int j = chapter[i];
+                    chap[j]++;
                     n++;
                 }
             }
-            updateProgression(chapter);
+            
             return n;
+         //   updateProgression(chap);
         } else {
             System.err.println("arrays size incorrect");
             return -1;
         }
+      
     }
 
     public int validateChapterTest(int answers[], int corrects[], int chapter, Progression pro) {
@@ -32,7 +37,7 @@ public class ValidateTestResult {
                 }
             }
             if (n > 3) {
-//        updateProgressionAfterChapter(chapter, pro); //verify if can be the same funtion or another one
+        updateProgressionAfterChapter(chapter, pro); //verify if can be the same funtion or another one
             }
             return n;
         } else {
@@ -44,37 +49,17 @@ public class ValidateTestResult {
     private void updateProgression(int chapters[]) {
         int array[] = null;
         for (int i = 0; i < chapters.length; i++) {
+            //   if(chapters[i]!=0)
+                    
             //incomplete 
+            
             //verify if there is a duplicate number in the arrar, 
             //if there is uses that number to update progression
-        }
+                    }
     }
     
     private void updateProgressionAfterChapter(int chapters, Progression pro) {
-        //bla bla bla
+         //update the chapter progression array until the chapters done
+        
+             pro.setChapter(chapters);}
     }
-}
-
-
-//    public void UpdateProgression(int[]i,int[]j,int k,Progression pro,Question ques ){
-//        int correct;
-//        int questCorrect;
-//        int total;
-//        if(ValidateResults(i[],j[],k,pro,ques) == 1)
-//            
-//            for(int i = 0;i< 9 ;i++)
-//                if(0==chapter[i])
-//                    pro.setChapter(chapter+1);
-//}
-//        //Function to evaluation test
-//        int correct;
-//        int questCorrect;
-//        int total;
-//        int n = 0;
-//        questCorrect = ques.getCorrect();
-//        
-//        correct = pro.getCorrect();
-//        total = pro.getTotal();
-//        
-//        if(correct > total/2)
-//                n = 1;
