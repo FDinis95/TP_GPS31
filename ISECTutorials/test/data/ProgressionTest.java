e2a5112a81f8717d5777f28fcbade5e3fc542600
simaohsferreira <simaohsferreira@gmail.com>
2017-12-05 19:17:35 +0000

    update to verifyChapterTest, added ProgressionTests

---
+++ ISECTutorials/test/data/ProgressionTest.java
@@ -0,0 +1,104 @@
+package data;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ProgressionTest {
+
+    Progression pro = new Progression();
+    int array[] = new int[10];
+    int array2[] = new int[10];
+
+    @Before
+    public void setUp() {
+        for (int i = 0; i < 10; i++) {
+            array2[i] = 1;
+        }
+    }
+
+    @Test
+    public void validateProgression() {
+        assertNotNull(pro);
+    }
+
+    @Test
+    public void validatGetChapterTests() {
+        assertNotNull(pro.getChapterTests());
+    }
+
+    @Test
+    public void validatGetChapter() {
+        assertNotNull(pro.getChapter());
+    }
+
+    @Test
+    public void validatGetCorrect() {
+        assertNotNull(pro.getCorrect());
+    }
+
+    @Test
+    public void validatGetTotalNumberOfQuestionsAnswerd() {
+        assertNotNull(pro.getTotalNumberOfQuestionsAnswerd());
+    }
+
+    @Test
+    public void validatGetChapterTestsIsZeros() {
+        assertArrayEquals(pro.getChapterTests(), array);
+    }
+
+    @Test
+    public void validatGetChapterIsZeros() {
+        assertArrayEquals(pro.getChapter(), array);
+    }
+
+    @Test
+    public void validatGetCorrectIsZero() {
+        assertEquals(pro.getCorrect(), 0);
+    }
+
+    @Test
+    public void validatGetTotalIsZeros() {
+        assertEquals(pro.getTotalNumberOfQuestionsAnswerd(), 0);
+    }
+
+    @Test
+    public void validatGetChapterTestsDiferent() {
+        assertNotEquals(array, pro.getChapterTests());
+    }
+
+    @Test
+    public void validatGetChapterDiferent() {
+
+        assertNotEquals(array, pro.getChapter());
+    }
+
+    @Test
+    public void validatSetChapterTestsEqual() {
+        for (int i = 0; i < 10; i++) {
+            pro.setChapterTests(i);
+        }
+        assertArrayEquals(array2, pro.getChapterTests());
+    }
+
+    @Test
+    public void validatSetChapterEqual() {
+        for (int i = 0; i < 10; i++) {
+            pro.setChapter(i);
+        }
+        assertArrayEquals(array2, pro.getChapter());
+    }
+
+    @Test
+    public void validatSetCorrectEqual() {
+        pro.setCorrect(10);
+        assertEquals(10, pro.getCorrect());
+    }
+
+    @Test
+    public void validatSetTotalEqual() {
+        pro.setTotalNumberOfQuestionsAnswerd(10);
+        assertEquals(10, pro.getTotalNumberOfQuestionsAnswerd());
+    }
+
+}
--- ISECTutorials/test/logic/VerifyChapterTest.java
+++ ISECTutorials/test/logic/VerifyChapterTest.java
@@ -1,7 +1,7 @@
 package logic;
 
 import data.Progression;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -9,24 +9,50 @@ public class VerifyChapterTest {
 
     Progression pro = new Progression();
     VerifyChapter ver = new VerifyChapter();
+    int array[] = new int[10];
+    int array2[] = new int[10];
 
     @Before
     public void setUp() {
-
+        for (int i = 0; i < 10; i++) {
+            array2[i] = 1;
+        }
     }
 
     @Test
     public void validateProgression() {
-        for (int i = 0; i < 10; i++) {
-            pro.setChapterTests(i);
-        }
         assertNotNull(pro);
-        assertNotNull(pro.getChapterTests());
     }
 
     @Test
-    public void validateNullChaptersProgression() {
+    public void getUnlockedChaptersNotNull() {
+        assertNotNull(ver.getUnlockedChapters(pro));
+    }
+
+    @Test
+    public void getUnlockedChaptersIfProgressionIsNull() {
         assertNotNull(ver.getUnlockedChapters(null));
+    }
+
+    @Test
+    public void getUnlockedChaptersValues() {
+        assertArrayEquals(ver.getUnlockedChapters(pro), array);
+    }
+
+    @Test
+    public void getUnlockedChaptersValuesNotZero() {
+        for (int i = 0; i < 10; i++) {
+            pro.setChapterTests(i);
+        }
+        assertNotEquals(ver.getUnlockedChapters(pro), array);
+    }
+
+    @Test
+    public void getUnlockedChaptersValuesIsEqual() {
+        for (int i = 0; i < 10; i++) {
+            pro.setChapterTests(i);
+        }
+        assertArrayEquals(ver.getUnlockedChapters(pro), array2);
     }
 
 }
