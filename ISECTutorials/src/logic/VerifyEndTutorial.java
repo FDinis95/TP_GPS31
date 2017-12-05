214174e1e7bac574f6558e528caa8f15b2e42e63
simaohsferreira <simaohsferreira@gmail.com>
2017-12-05 02:10:28 +0000

    update to verifyEndTutorial.

--- ISECTutorials/src/logic/VerifyEndTutorial.java
+++ ISECTutorials/src/logic/VerifyEndTutorial.java
@@ -1,33 +1,41 @@
-
 package logic;
 
 import data.Progression;
-        
+
 public class VerifyEndTutorial {
-    private int completed[];
-    private int done;
-//    Progression p() = new Progression;
-//    
-//    VerifyEndTutorial(){
-//        this.completed= new int[10];    }
-//
-//
-//    public VerifyEndTutorial(int[] completed, int done, progression p) {
-//        this.completed = completed;
-//        this.done = done;
-//        this.p = p;
-//    }
-//    public boolean isOver(){
-//        this.completed = .GetChapterTests//get do Progression criado no validate tests
-//        int done = 0;
-//        for (int i=0;i<10;i++)
-//            if (completed[i]==1)
-//                done++;
-//        
-//        if (done==10)
-//            return 1;
-//                    else
-//            return 0;
-//    }
+
+    //não podemos udar o construtor porque é precido devolver alguma coisa
     
+    public boolean verify(Progression pro) {
+        
+        //if bla bla bla
+
+        return true;
+    }
 }
+
+//    private int completed[];
+//    private int done;
+////    Progression p() = new Progression;
+////    
+////    VerifyEndTutorial(){
+////        this.completed= new int[10];    }
+////
+////
+////    public VerifyEndTutorial(int[] completed, int done, progression p) {
+////        this.completed = completed;
+////        this.done = done;
+////        this.p = p;
+////    }
+////    public boolean isOver(){
+////        this.completed = .GetChapterTests//get do Progression criado no validate tests
+////        int done = 0;
+////        for (int i=0;i<10;i++)
+////            if (completed[i]==1)
+////                done++;
+////        
+////        if (done==10)
+////            return 1;
+////                    else
+////            return 0;
+////    }
