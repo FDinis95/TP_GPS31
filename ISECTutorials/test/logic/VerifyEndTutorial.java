46819d4f091e5dde646711e90be257d1e01aecce
Ragmarok <hugo.alex.santos.93@gmail.com>
2017-12-05 18:17:45 +0000

    Test for VerfiryEndTutorial

---
+++ ISECTutorials/test/logic/VerifyEndTutorial.java
@@ -0,0 +1,35 @@
+package logic;
+
+import data.Progression;
+import static org.junit.Assert.assertNotNull;
+import org.junit.Before;
+import org.junit.Test;
+
+public class VerifyEndTutorial {
+
+    Progression pro = new Progression();
+    VerifyEndTutorial ver = new VerifyEndTutorial();
+    
+   /* @Before
+    public void setUp() {
+
+    }*/
+
+//    @Test
+//    public void progressionNotNull() {
+//   // int completedChapters[] = pro.getChapterTests();
+//   // for (int i = 0; i < 10; i++) {
+//   //        if (completedChapters[i] == 0) {
+//   //            return false;
+//   //        }
+//   // }
+//        assertNotNull(pro);
+//        //assertNotNull(pro.getChapterTests());
+//        //return true;
+//    }
+////
+////    @Test
+////    public void validateNullEndTutorial() {
+////        assertNotNull(ver.verify(null));
+////    }
+}
