e581a0e58821585cd9f85fdadefca5c0b2d851ea
Ragmarok <hugo.alex.santos.93@gmail.com>
2017-12-05 17:26:02 +0000

    VerifyEndTutorial Completed

--- ISECTutorials/src/logic/VerifyEndTutorial.java
+++ ISECTutorials/src/logic/VerifyEndTutorial.java
@@ -4,38 +4,14 @@ import data.Progression;
 
 public class VerifyEndTutorial {
 
-    //não podemos udar o construtor porque é precido devolver alguma coisa
-    
     public boolean verify(Progression pro) {
-        
-        //if bla bla bla
 
+        int completedChapters[] = pro.getChapterTests();
+        for (int i = 0; i < 10; i++) {
+            if (completedChapters[i] == 0) {
+                return false;
+            }
+        }
         return true;
     }
 }
-
-//    private int completed[];
-//    private int done;
-////    Progression p() = new Progression;
-////    
-////    VerifyEndTutorial(){
-////        this.completed= new int[10];    }
-////
-////
-////    public VerifyEndTutorial(int[] completed, int done, progression p) {
-////        this.completed = completed;
-////        this.done = done;
-////        this.p = p;
-////    }
-////    public boolean isOver(){
-////        this.completed = .GetChapterTests//get do Progression criado no validate tests
-////        int done = 0;
-////        for (int i=0;i<10;i++)
-////            if (completed[i]==1)
-////                done++;
-////        
-////        if (done==10)
-////            return 1;
-////                    else
-////            return 0;
-////    }
