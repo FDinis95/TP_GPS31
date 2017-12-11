952a6c18ab6f4858dc981667cd94388ab96f4014
simaohsferreira <simaohsferreira@gmail.com>
2017-12-11 12:37:15 +0000

    Updated tests for ValidateTestResult

--- ISECTutorials/src/data/LoadQuestions.java
+++ ISECTutorials/src/data/LoadQuestions.java
@@ -83,6 +83,7 @@ public class LoadQuestions
             }
             Question q = new Question(array);
             question.add(q);
+//            question.add(new Question(array));
             br.close();
         } catch (FileNotFoundException ex)
         {
