1668deb0e60b36aa42743c54474c41fffbbfe447
simaohsferreira <simaohsferreira@gmail.com>
2017-12-17 20:51:15 +0000

    helper updates

---
+++ ISECTutorials/src/ui/Helper.java
@@ -0,0 +1,50 @@
+package ui;
+
+import data.Progression;
+
+public class Helper
+{
+
+    String[] stars;
+    int[] chapters;
+    int var;
+
+    public Helper()
+    {
+    }
+
+    public String[] fillStars(Progression pro)
+    {
+        stars = new String[5];
+        chapters = pro.getChapter();
+        var=0;
+        for (int i = 0; i < 10; i++)
+        {
+            var += chapters[i];
+        }
+        for (int j = 0; j < 5; j++)
+        {
+            if (var == 0)
+            {
+                stars[j] = "images/star-outline.png";
+            }
+            if (var == 1)
+            {
+                stars[j] = "images/star-half.png";
+                var -= 1;
+            }
+            else if (var >= 2)
+            {
+                stars[j] = "images/star.png";
+                var -= 2;
+            }
+//            System.out.println(stars[j]);
+        }
+//        "images/star.png"
+//        "images/star-half.png"
+//        "images/star-outline.png"
+            return stars;
+        
+    }
+
+}
